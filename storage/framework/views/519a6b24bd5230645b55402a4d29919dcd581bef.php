3cd67eda24924287e55d10f8a99abb9a4b2d4941
KinderJoy <43001980+dandirifaldi@users.noreply.github.com>
2021-06-01 22:26:38 +0700

    Delete storage directory

--- storage/framework/views/519a6b24bd5230645b55402a4d29919dcd581bef.php
+++
@@ -1,7 +0,0 @@
-
-<?php $__env->startSection('title','Users'); ?>
-
-<?php $__env->startSection('content'); ?>
-	<h1>Halaman Users</h1>	
-<?php $__env->stopSection(); ?>
-<?php echo $__env->make('layout.v_template', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH D:\Xampp\htdocs\lab\resources\views/v_users.blade.php ENDPATH**/ ?>
--- storage/framework/views/a7c75cfbc0f9a1d902453b4e185e28ca403366e9.php
+++
@@ -1,7 +0,0 @@
-
-<?php $__env->startSection('title','Reports'); ?>
-
-<?php $__env->startSection('content'); ?>
-	<h1>Halaman Report</h1>	
-<?php $__env->stopSection(); ?>
-<?php echo $__env->make('layout.v_template', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH D:\Xampp\htdocs\lab\resources\views/v_report.blade.php ENDPATH**/ ?>
