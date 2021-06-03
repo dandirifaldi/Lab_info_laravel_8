379d6dee26fbd536f96fcdbaf0f507f8d002c342
dandirifaldi <dandir86@gmail.com>
2021-06-03 22:36:57 +0700

    update route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LicenseController;
+use App\Http\Controllers\UsersController;
+use App\Http\Controllers\ReportController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,9 +17,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('v_home');
-});
-route::view('/license', 'v_license');
-route::view('/users', 'v_users');
-route::view('/report', 'v_report');
+Route::get('/', [HomeController::class, 'index']);
+Route::get('/license', [LicenseController::class, 'index']);
+Route::get('/report', [ReportController::class, 'index']);
+Route::get('/users', [UsersController::class, 'index']);
